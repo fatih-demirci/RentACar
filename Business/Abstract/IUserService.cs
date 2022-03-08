36027d769ce8912490c24286d99e35a108058359
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2022-03-08 15:16:42 +0300

    public access modifier

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Business.Abstract
 {
-    interface IUserService
+    public interface IUserService
     {
         IDataResult<List<User>> GetAll();
         IDataResult<User> GetById(int id);
