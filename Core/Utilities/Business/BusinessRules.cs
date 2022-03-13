f1341b9e14c4088f13161565aad477980629143c
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2022-03-13 23:05:04 +0300

    moved

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static List<IResult> Run(params IResult[] logics) {
+            List<IResult> result = new ();
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    result.Add(logic);
+                }
+            }
+            return result;
+        }
+
+    }
+}
