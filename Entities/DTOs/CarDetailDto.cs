ddc49c28013decc71942726a20e4760072808b38
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2022-04-02 11:53:56 +0300

    CarDetailDto update

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -8,9 +8,11 @@ namespace Entities.DTOs
 {
     public class CarDetailDto
     {
+        public int Id { get; set; }
         public string CarDescription { get; set; }
         public string BrandName { get; set; }
         public string ColorName { get; set; }
         public double DailyPrice { get; set; }
+        public int ModelYear { get; set; }
     }
 }
